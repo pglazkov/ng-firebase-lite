34fe4e3f629f9a59cca9b7f9a81a3e53c79e981f
Pavlo Glazkov <p.glazkov@gmail.com>
2018-05-17 07:44:08 +0200

    chore: support firebase 5 and built with Angular 6

--- src/firebase-app.ts
+++ src/firebase-app.ts
@@ -6,7 +6,8 @@ export abstract class FirebaseApp implements firebase.app.App {
     abstract auth: () => firebase.auth.Auth;
     abstract database: () => firebase.database.Database;
     abstract messaging: () => firebase.messaging.Messaging;
-    abstract storage: () => firebase.storage.Storage;
+    abstract storage: (url?: string) => firebase.storage.Storage;
     abstract delete: () => Promise<any>;
     abstract firestore: () => firebase.firestore.Firestore;
+    abstract functions(): firebase.functions.Functions;
   }
